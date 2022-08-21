ce1c284e1fcf95b2e8d22b37f65827bb1962f598
ademclk <ademonurcelik@icloud.com>
2022-08-21 19:53:10 +0300

    Framer motion added for feedback list.

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,18 +1,29 @@
-import FeedbackItem from './FeedbackItem';
-import PropTypes from 'prop-types';
+import FeedbackItem from './FeedbackItem'
+import PropTypes from 'prop-types'
+
+import { motion, AnimatePresence } from 'framer-motion'
 
 function FeedbackList({ feedbacks, handleDelete }) {
   if (!feedbacks || feedbacks.length === 0) {
-    return <p>Out of feedbacks, please try again later.</p>;
+    return <p>Out of feedbacks, please try again later.</p>
   }
 
   return (
     <div className='feedback-list'>
-      {feedbacks.map((item) => (
-        <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
-      ))}
+      <AnimatePresence>
+        {feedbacks.map((item) => (
+          <motion.div
+            key={item.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <FeedbackItem key={item.id} item={item} handleDelete={handleDelete} />
+          </motion.div>
+        ))}
+      </AnimatePresence>
     </div>
-  );
+  )
 }
 
 FeedbackList.propTypes = {
@@ -23,6 +34,6 @@ FeedbackList.propTypes = {
       text: PropTypes.string.isRequired,
     })
   ).isRequired,
-};
+}
 
-export default FeedbackList;
+export default FeedbackList
